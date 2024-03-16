73a4fc3df8a0a5f8bb370429d443e70d33374616
joao-milhomem <milhomemjoao.work@gmail.com>
2024-03-16 11:14:21 -0300

    install and config styled components

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import { Router } from './routes/index'
 
 import { DefaultLayout } from './layouts/DefaultLayout'
 
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/theme'
+import GlobalStyle from './styles/global'
+
 export function App() {
   return (
     <BrowserRouter>
-      <DefaultLayout />
-      <Router />
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <DefaultLayout />
+        <Router />
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
